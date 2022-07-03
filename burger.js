03708db7476dd1843a4f7377258304d70df4818a
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-07-03 21:05:48 +0300

    mobile version pt1

---
+++ burger.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+    $('.header__menu_item').click(function(eVent) {
+        $('.header__burger,.header__menu').removeClass('active');
+    })
+})
